83bdae3bcee05b360f01e8593969658689d2642f
Wladimir J. van der Laan <laanwj@gmail.com>
2017-01-04 15:05:57 +0100

    build_bench: add sse42 option

--- build_bench.sh
+++ build_bench.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 PLATFORM=$1
 if [ -z "$PLATFORM" ]; then
-    echo "Must specify platform (arm)"
+    echo "Must specify platform (arm or sse42)"
     exit 1
 fi
 ARGS=""
 if [ "$PLATFORM" == "arm" ]; then
     ARGS="-march=armv8-a+crc -DBENCH_ARM"
+fi
+if [ "$PLATFORM" == "sse42" ]; then
+    ARGS="-msse4.2 -DBENCH_SSE42"
 fi
 g++ crcbench.cc crc32c.cc perf.cpp -O2 -o crcbench -std=c++11 ${ARGS}
 
